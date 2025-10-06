011b7718122b44ff55ad1acc8ca9d72f79c27f87
freaky-hue <pip8836@gmail.com>
2025-10-06 14:19:18 +0100

    teste

---
+++ dragFiles.js
@@ -0,0 +1,27 @@
+module.exports.dragFiles = function (parent) {
+    var obj = {};
+
+    obj.parent = parent;
+    obj.meshServer = parent.parent;
+    obj.debug = obj.meshServer.debug;
+
+
+
+    obj.server_startup = function () {
+        obj.meshServer.pluginHandler.scripttask_db = require(__dirname + '/db.js').CreateDB(obj.meshServer);
+        obj.db = obj.meshServer.pluginHandler.scripttask_db;
+        obj.resetQueueTimer();
+    };
+
+
+    obj.gpD = function () {
+        const devicesGroups = parent.ListDeviceGroups();
+
+        devicesGroups.forEach(element => {
+            
+        });
+    }
+
+    return obj
+
+}
